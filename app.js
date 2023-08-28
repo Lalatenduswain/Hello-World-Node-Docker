102283ddd0b4ba23fb5097e5f9f35b2b695a2283
Lalatendu Keshari Swain <lkswain_2003@yahoo.com>
2023-08-29 00:13:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+const express = require('express');
+const app = express();
+
+const PORT = 8080; // Change to the desired port
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
